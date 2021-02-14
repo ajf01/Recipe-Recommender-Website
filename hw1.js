b998829646e10540f98ba57c791ef1f8e81e10ec
ajf010 <ajf010@ucsd.edu>
2021-02-13 16:38:45 -0800

    Testing Interactiveness

--- hw1.js
+++
@@ -1,8 +0,0 @@
-function showDialog() {
-  var dialog = document.querySelector('dialog');
-  console.log('Fired');
-  dialog.show();
-  setTimeout(function () {
-      dialog.close();
-    }, 3000);
-}
