979024575a89af3b1cda707de2e2a2da47803bd3
ajf01 <45701597+Ajf010@users.noreply.github.com>
2021-02-20 15:34:59 -0800

    Flask Demo

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 #    return "Hello Team!"
-    return render_template('index.html')
+#    return render_template('index.html')
+    return render_template('example.html')
 
 #if __name__ == "__main__":
 #    app.run(debug=True)
