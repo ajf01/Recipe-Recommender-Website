8b5dfe282b409ceb7a9ea0bd6116e9af8f4bca8c
ajf01 <45701597+Ajf010@users.noreply.github.com>
2021-02-20 16:06:57 -0800

    Created Flask Template In app.py File

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#db = SQLAlchemy(app)
+
+#class Todo(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    content = db.Column(db.String(200), nullable=False)
+#    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+#    def __repr__(self):
+#        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST','GET'])
 def hello():
 #    return "Hello Team!"
 #    return render_template('index.html')
